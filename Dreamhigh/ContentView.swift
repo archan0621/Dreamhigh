5d2a62edc2e9d67e198ae5093ef44b3398d0dc05
박종하 <joshua_0621@naver.com>
2026-01-02 16:57:29 +0900

    Initial commit

---
+++ Dreamhigh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dreamhigh
+//
+//  Created by 박종하 on 1/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dreamhigh/DreamhighApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamhighApp.swift
+//  Dreamhigh
+//
+//  Created by 박종하 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DreamhighApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
